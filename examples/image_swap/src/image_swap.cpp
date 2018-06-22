cbb80d7d25ec7d1b156e23865728fe0e52c3cb90
Peter Bindels <peter.bindels@tomtom.com>
2018-06-22 23:22:12 +0200

    Add image swap demo. It loads 'myimage.png' and then swaps pixels horizontally pairwise repeatedly.

---
+++ examples/image_swap/src/image_swap.cpp
@@ -0,0 +1,26 @@
+#include "pixel.hh"
+#include <thread>
+#include <chrono>
+using namespace std::literals::chrono_literals;
+using namespace Pixel;
+
+static const int npoint = 50;
+
+int main() {
+    Window mainWindow(640, 480, "Hello world!");
+    Canvas canvas("myimage.png");
+
+    while (!mainWindow.shouldExit()) {
+        for(int j=0; j < npoint; ++j)
+        {
+            int x1 = rand() % (canvas.width() - 1);
+            int y1 = rand() % canvas.height();
+            Color a = canvas.get(x1, y1);
+            Color b = canvas.get(x1+1, y1);
+            canvas.plot(x1, y1, b);
+            canvas.plot(x1+1, y1, a);
+        }
+        mainWindow.show(canvas);
+    }
+}
+
