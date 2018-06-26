c9c5da9432ccaa60ed917a4944f84df26d47cb6e
Peter Bindels <peter.bindels@tomtom.com>
2018-06-26 23:17:30 +0200

    Added effect interface

--- pixel/include/pixel/Window.hpp
+++ pixel/include/pixel/Window.hpp
@@ -8,11 +8,26 @@ namespace Pixel {
   class Canvas;
   class Subcanvas;
 
+  enum Effect {
+    None,
+    Spiral,
+    VerticalWaves,
+    HorizontalWaves,
+    Blocking,
+    Shaking,
+    Blur,
+  };
+
+  inline Effect get_random_effect() {
+    return Effect((rand() % 6) + 1);
+  }
+
   class Window {
   public:
     Window(uint32_t width, uint32_t height, std::string name);
     ~Window();
     void set_base_scale(float base) { baseScale = base; }
+    void set_effect(Effect type = get_random_effect(), float strength = 1.0f);
     bool shouldExit();
     void show(const Subcanvas& canvas);
     void draw(const Subcanvas& canvas, uint32_t x, uint32_t y, float scale = 1, float rotation = 0);
